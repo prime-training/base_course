38b6f7fec756776f19617c626253980aad0d8b8e
briancsparks <briancsparks@gmail.com>
2014-03-01 09:29:59 -0800

    changed tabs to spaces

--- primes/primes/primes.cpp
+++ primes/primes/primes.cpp
@@ -9,28 +9,28 @@ char key[1024];
 
 int main(int argc, char* argv[])
 {
-	int print_threshold = 10000, threshold_interval = 10000;
+  int print_threshold = 10000, threshold_interval = 10000;
 
-	for (int i = 0; i < 300000; i++) {
-		if (is_prime(i)) {
-			if (i > print_threshold) {
-				printf("%d\n", i);
-				print_threshold += threshold_interval;
-			}
-		}
-	}
+  for (int i = 0; i < 300000; i++) {
+    if (is_prime(i)) {
+      if (i > print_threshold) {
+        printf("%d\n", i);
+        print_threshold += threshold_interval;
+      }
+    }
+  }
 
-//	printf("Press Enter to continue\n");
-//	getchar();
-	return 0;
+//  printf("Press Enter to continue\n");
+//  getchar();
+  return 0;
 }
 
 bool is_prime(int n)
 {
-	for (int i = 2; i < n; ++i) {
-		if (n % i == 0) {
-			return false;
-		}
-	}
-	return true;
+  for (int i = 2; i < n; ++i) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
