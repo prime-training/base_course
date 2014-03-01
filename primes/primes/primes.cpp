fbe9604c46e14264e2257726694d2c84ce7e74f9
briancsparks <briancsparks@gmail.com>
2014-03-01 09:44:02 -0800

    reorganized to make it cleaner

--- primes/primes/primes.cpp
+++ primes/primes/primes.cpp
@@ -3,34 +3,63 @@
 
 #include <stdio.h>
 
-bool is_prime(int);
+// What is the largest number to look at?
+#define LARGEST 150000
 
-char key[1024];
-
-int main(int argc, char* argv[])
-{
-  int print_threshold = 10000, threshold_interval = 10000;
-
-  for (int i = 0; i < 300000; i++) {
-    if (is_prime(i)) {
-      if (i > print_threshold) {
-        printf("%d\n", i);
-        print_threshold += threshold_interval;
-      }
-    }
-  }
-
-//  printf("Press Enter to continue\n");
-//  getchar();
-  return 0;
-}
-
+// Is the number a prime?
 bool is_prime(int n)
 {
-  for (int i = 2; i < n; ++i) {
+  // Look at all the numbers between 2 and n
+  int i = 2;
+  while (i < n) {
+
+    // If it devides evenly, it is not a prime '%' means remainder
     if (n % i == 0) {
       return false;
     }
+
+    // Look at the next number
+    i++;
   }
+
+  // If we get here, the number cannot be divided by any of the numbers
+  // less that it, so it is a prime
   return true;
 }
+
+/**
+ *  The main function
+ */
+int main(int argc, char* argv[])
+{
+  // Only print when the number goes above the threshold, for each interval
+  int print_threshold = 10000, threshold_interval = 10000;
+
+  // How many primes have we found?
+  int count = 0;
+
+  // Loop over the potential primes
+  int i = 2;
+  while (i < LARGEST) {
+
+    // Test for prime-ness
+    if (is_prime(i)) {
+
+      // We've found one!
+      count = count + 1;
+
+      // Print, but only some of them
+      if (i > print_threshold) {
+        printf("%7d -- %d\n", count, i);
+        print_threshold += threshold_interval;
+      }
+    }
+
+    i++;
+  }
+
+  //printf("Press Enter to continue\n");
+  //getchar();
+  return 0;
+}
+
